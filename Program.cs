e8dbcf59b8f26800db581ac7f60db9f3e5231f66
paulb <paulb@PAULBLANDON31>
2023-08-28 17:08:00 -0600

    12

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Mimetodo();
+
+void Mimetodo() 
+{
+    Console.WriteLine("AHORA TENGO UN METODO");
+}
+
 Console.WriteLine("Hola sam!");
 
 
