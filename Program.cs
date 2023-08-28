6e60eeac8247dd9ed1a303f74461ba21d13dbebb
paulb <paulb@PAULBLANDON31>
2023-08-28 16:57:52 -0600

    Revert "un msj"
    This reverts commit 3eabeeee3a5a889590f17dbfe40edc9a657e82ab.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hola sam!");
-
+Console.WriteLine("Hola sam!");
+Console.WriteLine("Hola sam!");
+Console.WriteLine("Hola sam!");
+Console.WriteLine("Hola sam!");
+Console.WriteLine("Hola sam!");
+Console.WriteLine("Hola sam!");
 
