3eabeeee3a5a889590f17dbfe40edc9a657e82ab
samantazel04 <143428390+samantazel04@users.noreply.github.com>
2023-08-28 16:44:06 -0600

    un msj

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hola sam!");
-Console.WriteLine("Hola sam!");
-Console.WriteLine("Hola sam!");
-Console.WriteLine("Hola sam!");
-Console.WriteLine("Hola sam!");
-Console.WriteLine("Hola sam!");
-Console.WriteLine("Hola sam!");
+
 
